3a6b78f663e4bac4b32f0de1ac321de594270598
unknown <krithika.gn@gmail.com>
2019-06-12 20:28:58 -0400

    my  class

---
+++ javascript/Javascript-exercise/if-statement.js
@@ -0,0 +1,8 @@
+var fruit;
+fruit='orange';
+var x;
+x=fruit.length;
+if(x>5){
+    console.log('The fruit name has more than five characters.');
+}else{console.log('The fruit name has five characters or less.');
+}
---
+++ javascript/Javascript-exercise/object-properties.js
@@ -0,0 +1,7 @@
+var food = {
+// Declare a global variable //
+    types: "only pizza"
+//Use Object properties //
+}
+console.log(food.types);
+//Print the output//
