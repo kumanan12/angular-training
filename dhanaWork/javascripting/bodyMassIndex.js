248367e25a5c087d1242174d63f3fb79c8ecef6e
dhanalakshmimohankumar <dhana.webdev@gmail.com>
2019-06-15 12:26:23 -0400

    dhana Javascript Work

---
+++ dhanaWork/javascripting/bodyMassIndex.js
@@ -0,0 +1,5 @@
+function bodyMassIndex(weight,height){
+    return weight/(height*height)
+};
+
+console.log(bodyMassIndex(53,5.5));
---
+++ dhanaWork/javascripting/undeclaredVariable.js
@@ -0,0 +1,8 @@
+function baz(foo){
+   foo = "dhana";
+   bam = "yak";
+   console.log(foo);
+}
+baz();
+console.log(bam);
+//console.log(foo); 
