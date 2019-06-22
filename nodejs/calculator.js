15f13d6e4df19bdd957e2d4934b3684aac4e90be
Kumanan Murugesan <kumananmurugesan@kumanans-mbp.lan>
2019-06-21 22:42:06 -0400

    June-19 in class

--- nodejs/calculator.js
+++ nodejs/calculator.js
@@ -1,12 +1,31 @@
 // public function add
 
 var xyz = 10;
+ var addCounter = 0;
 
+ exports.xyz = xyz;
+
+ exports.config = {
+     url: ''
+ };
+ 
+function counter() {
+    addCounter ++;
+    console.log(`add is called ${addCounter} times`);
+}
+
+function someAdd(n1, n2) {
+    
+    return n1 + n2;
+}
 
 exports.add = function (n1, n2) {
+    counter();
     return n1 + n2;
 };
 
 exports.multiply = function(n1, n2) {
     return n1 * n2;
 };
+
+exports.yyy= someAdd;
