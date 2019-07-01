c8e92ecb114e76ab336e14b7770dcab2f7959aa2
unknown <krithika.gn@gmail.com>
2019-06-30 20:18:27 -0400

    Krithika-june30

---
+++ javascript/Krithika/Refactored.js
@@ -0,0 +1,20 @@
+function bark(dogName,dogWeight){
+    if (dogWeight>20){
+   
+         console.log(dogName + ' says WOOF WOOF');
+    } else
+    {
+        console.log(dogName+ ' says woof woof');
+}
+}
+bark('Rover','23');
+bark('Spot','13');
+bark('Spike','53');
+bark('Lady','17');
+var name = "Simba";
+var weight = 23;
+
+bark(name,weight);
+
+     
+
